bea476ffc8c7f5deee6b4f11cd5f05a2aa8ab50c
Reese Kunz <reesekunz21@gmail.com>
2019-09-16 13:08:28 -0700

    register post working with hash

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,8 +1,11 @@
 const bcrypt = require("bcryptjs");
 
 const Users = require("../helpers/usersModel");
+
 // If the user is logged in, respond with an array of all the users contained in the database.
 // If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
+// if credentials are valid, let request continue, if invalid, return 401
+// use the middleware to restrict access to the GET endpoint /users
 
 module.exports = (request, response, next) => {
   let { username, password } = request.headers;
@@ -13,11 +16,11 @@ module.exports = (request, response, next) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         next();
       } else {
-        res.status(401).json({ message: "You shall not pass!" });
+        response.status(401).json({ message: "You shall not pass!" });
       }
     })
     .catch(error => {
-      res.status(500).json(error);
+      response.status(500).json(error);
     });
 };
 // This is how you would set up the headers with axios
--- register/registerRouter.js
+++ register/registerRouter.js
@@ -1,18 +1,26 @@
 const express = require("express");
 
 const Users = require("../helpers/usersModel");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const db = require("../data/db-config");
 
 const router = express.Router();
 
-router.get("/", (request, response) => {
-  Users.getProjects()
-    .then(projects => {
-      response.json(projects);
+// POST to /api/register
+// 	Creates a user using the information sent inside the body of the request.
+// Hash the password before saving the user to the database.
+
+router.post("/", (request, response) => {
+  let { username, password } = request.body;
+
+  const hash = bcrypt.hashSync(password, 14); // it's 2 ^ 14, not 14. 2^14 is the number of rounds - higher the number, the more secure the hash will be (harder for someone to pregenerate a hash) - try to have the # at 14 or higher
+
+  Users.add({ username, password: hash })
+    .then(saved => {
+      response.status(201).json(saved);
     })
     .catch(error => {
-      response.status(500).json({ message: "Failed to get projects" });
+      response.status(500).json(error);
     });
 });
 
