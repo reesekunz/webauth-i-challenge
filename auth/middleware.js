c2979f747a853c962a9500e7f609370d0df475fb
Reese Kunz <reesekunz21@gmail.com>
2019-09-17 12:48:57 -0700

    added middleware and session config

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,37 +1,13 @@
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 
-const Users = require("../helpers/usersModel");
-
-// If the user is logged in, respond with an array of all the users contained in the database.
-// If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
-// if credentials are valid, let request continue, if invalid, return 401
-// use the middleware to restrict access to the GET endpoint /users
+// const Users = require("../helpers/usersModel");
 
 module.exports = (request, response, next) => {
-  let { username, password } = request.headers;
-
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        response.status(401).json({ message: "You shall not pass!" });
-      }
-    })
-    .catch(error => {
-      response.status(500).json(error);
-    });
+  // is the user logged in === do we have information about the user in our session
+  if (request.session && request.session.user) {
+    next();
+  } else {
+    response.status(401).json({ message: "You shall not pass!" });
+  }
 };
-// This is how you would set up the headers with axios
-
-// function fetch() {
-//   const reqOptions = {
-//     headers: {
-//       username: "",
-//       password: ""
-//     }
-//   };
-
-// axios.get(url, reqOptions).then().catch()
-// axios.post(url, data, reqOptions).then().catch()
+// middleware doesnt need to verify credentials anymore, just needs to check to see if the user has a session
--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const express = require("express");
 const usersRouter = require("./users/usersRouter");
 const registerRouter = require("./register/registerRouter");
 const loginRouter = require("./login/loginRouter");
+// day 2 - adding session (npm install express-session)
+const session = require("express-session");
+// (npm i connect-session-knex) => have to require the Store after the session - remember to pass session using currying
+// const KnexSessionStore = require("connect-session-knex")(session); => doing this in sessionConfig.js instead
+
+const sessionConfig = require("./sessionConfig");
 
 const server = express();
 
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
 server.use("/api/register", registerRouter);
